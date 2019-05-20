405b9e608dd2951504df9e56054d356ed926cdac
Henrik Wirth <henrik@kopfwelt.com>
2019-05-20 23:38:01 +0200

    Revert and setup storybook through vue cli

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,18 +7,18 @@ module.exports = async ({ config, mode }) => {
     // 'PRODUCTION' is used when building the static version of storybook.
 
     // Make whatever fine-grained changes you need
-    config.module.rules.push({
-        test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-    });
-
-    config.module.rules.push({
-        test: [/\.stories\.js$/, /index\.js$/],
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        include: [path.resolve(__dirname, '../')],
-        enforce: 'pre',
-    });
+    // config.module.rules.push({
+    //     test: /\.scss$/,
+    //     use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+    //     include: path.resolve(__dirname, '../'),
+    // });
+    //
+    // config.module.rules.push({
+    //     test: [/\.stories\.js$/, /index\.js$/],
+    //     loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    //     include: [path.resolve(__dirname, '../')],
+    //     enforce: 'pre',
+    // });
 
     // Return the altered config
     return config;
