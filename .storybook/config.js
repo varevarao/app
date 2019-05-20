ef2d484dff7352b0bd3035a7d1c726a5459197f9
Rijk van Zanten <rijkvanzanten@me.com>
2019-05-20 18:29:04 -0400

    Add globals

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from '@storybook/vue'
 
+import '../src/globals';
+import '../src/design/main.scss';
+
 const req = require.context('../../src', true, /.stories.js$/)
 
 function loadStories() {
