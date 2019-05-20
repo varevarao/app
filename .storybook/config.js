1c43f6780692f44ff9f745e9547d5b1b067a5636
Rijk van Zanten <rijkvanzanten@me.com>
2019-05-20 17:32:12 -0400

    Demo storybook setup

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configure } from '@storybook/vue'
+
+import "../src/globals";
+import "../src/design/main.scss";
+
+const req = require.context('../src', true, /.stories.js$/)
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
