fe4707639f4c06c75a458cb74b62809229e96fbc
Nitwel <nitwel@arcor.de>
2019-05-24 15:56:37 +0200

    - added proper display

--- src/interfaces/repeater/display.vue
+++ src/interfaces/repeater/display.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="no-wrap">{{ displayValue }}</div>
+  <v-popover trigger="hover" v-if="valueFields && valueFields.length > 0">
+    <div>{{ itemCount }}</div>
+
+    <template slot="popover">
+      <ul class="list">
+        <li v-for="(val, i) in value" :key="i">{{ val[valueFields[0]] }}</li>
+      </ul>
+    </template>
+  </v-popover>
+
+  <div v-else class="item-count">{{ itemCount }}</div>
 </template>
 
 <script>
@@ -8,15 +18,37 @@ import mixin from "@directus/extension-toolkit/mixins/interface";
 export default {
   mixins: [mixin],
   computed: {
-    displayValue() {
-      let value = this.value;
-
-      if (this.options.formatValue) {
-        value = this.$helpers.formatTitle(value);
-      }
-
-      return value;
+    valueFields() {
+      return this.$lodash
+        .chain(this.options.fields)
+        .pickBy(value => {
+          return value.hasOwnProperty("preview") && value.preview;
+        })
+        .keys()
+        .value();
+    },
+    itemCount() {
+      return this.$lodash.keys(this.value).length + " Items";
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.list {
+  max-height: 200px;
+  overflow-y: auto;
+
+  list-style: none;
+  padding: 0;
+
+  li {
+    color: var(--dark-gray);
+    padding: 8px 0;
+
+    &:not(:last-of-type) {
+      border-bottom: 1px solid var(--lightest-gray);
+    }
+  }
+}
+</style>
